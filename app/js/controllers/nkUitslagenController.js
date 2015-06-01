7c0bbcfc8ad84d283ea8b02618a1dba924f327c3
Han Markslag <han.markslag@xs4all.nl>
2015-06-01 18:03:30 +0200

    Changed order meerkamp parts

--- app/js/controllers/nkUitslagenController.js
+++ app/js/controllers/nkUitslagenController.js
@@ -9,7 +9,7 @@ angular.module('altisNKMasters').controller('nkUitslagenCTRL', ['$scope', 'nkLoa
         $scope.uitslagenGewichtwerpen = nkLoadUitslagenService.loadUitslagenGewichtwerpen();
         $scope.uitslagenWerpvijfkamp = nkLoadUitslagenService.loadUitslagenWerpvijfkamp();
 
-        $scope.onderdelen = ["NK Kogelslingeren","NK Gewichtwerpen","NK Werpvijfkamp","5K Kogel", "5K Speer", "5K Discus", "5K Kogelslingeren", "5K Gewichtwerpen"];
+        $scope.onderdelen = ["NK Kogelslingeren","NK Gewichtwerpen","NK Werpvijfkamp","5K Kogelslingeren", "5K Kogel", "5K Discus", "5K Speer", "5K Gewichtwerpen"];
         $scope.nkSelectOnderdeel = {onderdeel:"NK Kogelslingeren"};
         $scope.categorien = ["V35","V40","V45","V50","V55","V60","V65","V70","M35","M40","M45","M50","M55","M60","M65","M70","M80"];
         $scope.nkSelectCategorie = {categorie:'V35'};
